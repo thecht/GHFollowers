5583b30d8d5e2ce36c1ba4f3281a8a44e9303d72
Theodore Hecht <treyhecht@me.com>
2020-01-10 17:10:27 -0500

    Initial Commit

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Theodore Hecht on 1/10/20.
+//  Copyright © 2020 Theodore Hecht. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Theodore Hecht on 1/10/20.
+//  Copyright © 2020 Theodore Hecht. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
