fe4961c71f28667e6c06febe7f713f3df92a69ec
Theodore Hecht <treyhecht@me.com>
2020-01-13 13:22:27 -0500

    adds GFButton and GFTextField

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Theodore Hecht on 1/13/20.
+//  Copyright © 2020 Theodore Hecht. All rights reserved.
+//
+
+import UIKit
+
+public class GFButton: UIButton {
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
