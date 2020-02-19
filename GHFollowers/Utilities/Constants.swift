877ad1d856277c326c71d8b283e3ed78d2b65a18
Theodore Hecht <treyhecht@me.com>
2020-02-19 17:27:16 -0500

    adds user info child view controller

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Theodore Hecht on 2/19/20.
+//  Copyright © 2020 Theodore Hecht. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
