93b64f4feb78d784e53734e150679d8cae4aae9b
Theodore Hecht <treyhecht@me.com>
2020-02-24 12:09:35 -0500

    refactor scene delegate into custom tab bar class

---
+++ GHFollowers/Custom Views/TabBar Controllers/GFTabBarController.swift
@@ -0,0 +1,46 @@
+//
+//  GFTabBarController.swift
+//  GHFollowers
+//
+//  Created by Theodore Hecht on 2/24/20.
+//  Copyright © 2020 Theodore Hecht. All rights reserved.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UINavigationBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNavigationController(), createFavoritesNavigationController()]
+    }
+    
+    func createSearchNavigationController() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNavigationController() -> UINavigationController {
+        let favoritesVC = FavoritesListVC()
+        favoritesVC.title = "Favorites"
+        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesVC)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
